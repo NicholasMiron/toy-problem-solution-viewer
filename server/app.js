4f995f5fa745939c5cf181b6f030b475c923c98e
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-25 08:24:02 -0500

    Added better logging

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 // const db = require('../db/index.js');
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan('combined', { stream: fs.createWriteStream(path.join(__dirname, '../coverage/access.log'), { flags: 'a' }) }));
 app.use(cors());
 
 app.use(express.static('dist'));
