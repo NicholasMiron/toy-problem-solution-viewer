5f6d7bd14735a773a5d6ce6df8849590b0ad85ed
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-26 12:56:42 -0500

    Revert "Added better logging"
    This reverts commit 4f995f5fa745939c5cf181b6f030b475c923c98e.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
 
 // const db = require('../db/index.js');
 
 const app = express();
 
-app.use(morgan('combined', { stream: fs.createWriteStream(path.join(__dirname, '../coverage/access.log'), { flags: 'a' }) }));
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.static('dist'));
